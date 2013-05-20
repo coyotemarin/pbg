ec8eb90815a5ba2ca5d3101a87a236f2623cd019
David Marin <dm@davidmarin.org>
2013-05-19 23:17:30 -0700

    added __eq__() and __repr__() to Item

--- python/pbg/common/microdata.py
+++ python/pbg/common/microdata.py
@@ -5,7 +5,10 @@ import microdata
 
 class Item(microdata.Item):
     """Add an "extra" field to microdata Items, so people won't feel the need
-    to make up ad-hoc properties."""
+    to make up ad-hoc properties.
+
+    Also add __eq__() and __repr__().
+    """
 
     def __init__(self, *args, **kwargs):
         super(Item, self).__init__(*args, **kwargs)
@@ -19,3 +22,20 @@ class Item(microdata.Item):
             item['extra'] = self.extra
 
         return item
+
+    def __eq__(self, other):
+        if not isinstance(other, microdata.Item):
+            return False
+
+        return (self.itemid == other.itemid and
+                self.itemtype == other.itemtype and
+                self.props == other.props and
+                self.extra == getattr(other, 'extra', {}))
+
+    def __repr__(self):
+        return '%s(%r, %r, props=%r, extra=%r)' % (
+            self.__class__.__name__,
+           ' '.join(uri.string for uri in self.itemtype),
+           self.itemid,
+           self.props,
+           self.extra)
